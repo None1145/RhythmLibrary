36932d7785b2d7f51cb06b3009dc464c0a3b94ac
None1145 <hhrh1123@outlook.com>
2026-05-31 22:44:19 +0800

    fix(common): Add pathlib.Path support

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,10 @@
+import pathlib
 import msgpack
 
-def save_data_to_file(data: dict, save_path: str) -> None:
+def save_data_to_file(data: dict, save_path: str | pathlib.Path) -> None:
+    if isinstance(save_path, pathlib.Path):
+        save_path = str(save_path)
+    
     if not save_path.endswith(".msgpack"):
         save_path += ".msgpack"
     with open(save_path, "wb") as f:
