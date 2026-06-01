c861fdf6395aaaabb66355d082fc2cbc301acecf
None1145 <hhrh1123@outlook.com>
2026-06-01 23:29:11 +0800

    feat(common): Add TEMP_DIR

--- common/config.py
+++ common/config.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+TEMP_DIR = Path(__file__).resolve().parent.parent / "temp"
+TEMP_DIR.mkdir(parents=True, exist_ok=True)
